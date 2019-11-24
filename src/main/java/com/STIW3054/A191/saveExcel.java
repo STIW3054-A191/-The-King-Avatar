ade477f637f675406691e7aca61d18de0f37324e
PhuahMeiQi <meiqi1106@gmail.com>
2019-11-24 17:33:08 +0800

    set 1st Row Font Size: class saveExcel

--- src/main/java/com/STIW3054/A191/saveExcel.java
+++ src/main/java/com/STIW3054/A191/saveExcel.java
@@ -19,6 +19,17 @@ public class saveExcel {
             Row rowHeading = sheet.createRow(0);
             rowHeading.createCell(0).setCellValue("Link                                                               ");
 
+            //1st Row Font Size
+            for (int i = 0; i < 1; i++) {
+                CellStyle style = workbook.createCellStyle();
+                Font font = workbook.createFont();
+                font.setBold(true);
+                font.setFontName(HSSFFont.FONT_ARIAL);
+                font.setFontHeightInPoints((short) 12);
+                style.setFont(font);
+                style.setVerticalAlignment(VerticalAlignment.CENTER);
+                rowHeading.getCell(i).setCellStyle(style);
+            }
         }
 
     }
