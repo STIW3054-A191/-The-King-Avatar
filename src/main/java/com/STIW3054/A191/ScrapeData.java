4fe5d88538e74187db35039dc7ccbf42c1f0a1a0
sohcheefung <gmail@sohcheefung>
2019-11-25 00:50:03 +0800

    update by 259521

--- src/main/java/com/STIW3054/A191/ScrapeData.java
+++ src/main/java/com/STIW3054/A191/ScrapeData.java
@@ -1,16 +1,25 @@
 package com.STIW3054.A191;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.IOException;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
 public class ScrapeData
 {
-    public List<Data> findAll() {
+    public void findAll() {
         try {
             System.out.println("");
             String URL = "https://github.com/STIW3054-A191/Assignments/issues/1";
