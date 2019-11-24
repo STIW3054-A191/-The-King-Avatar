7fdfdb5c839abf72a3cb4a1e0c97f0b944684cfd
jasonway96 <limguangway96@gmail.com>
2019-11-24 13:58:41 +0800

    Update

--- src/main/java/com/STIW3054/A191/ScrapeData.java
+++ src/main/java/com/STIW3054/A191/ScrapeData.java
@@ -1,8 +1,17 @@
 package com.STIW3054.A191;
 
+import javax.lang.model.util.Elements;
+import javax.swing.text.Document;
+import java.io.IOException;
 import java.util.LinkedList;
 
 public class ScrapeData
 {
-
+    public static LinkedList<info> findAll() throws IOException
+    {
+        LinkedList<info> info = new LinkedList<info>();
+        final String url = "https://github.com/STIW3054-A191/Main-Issues/issues/1";
+        final Document file = Jsoup.connect(url).get();
+        Elements row = file.select("table");
+    }
 }
