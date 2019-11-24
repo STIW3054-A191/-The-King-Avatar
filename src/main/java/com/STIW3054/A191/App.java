f90667f9f4f2c793027534f5db412e970a0ac4f0
PhuahMeiQi <meiqi1106@gmail.com>
2019-11-24 17:39:57 +0800

    update code class App

--- src/main/java/com/STIW3054/A191/App.java
+++ src/main/java/com/STIW3054/A191/App.java
@@ -1,4 +1,11 @@
 package com.STIW3054.A191;
 
 public class App {
+    public void app() {
+        try {
+
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+    }
 }
