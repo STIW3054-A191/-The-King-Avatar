072bdf9940d53f84a2da663f3568a00d4ac33f92
PhuahMeiQi <meiqi1106@gmail.com>
2019-11-24 17:30:18 +0800

    create sheet: class saveExcel

--- src/main/java/com/STIW3054/A191/saveExcel.java
+++ src/main/java/com/STIW3054/A191/saveExcel.java
@@ -1,4 +1,20 @@
 package com.STIW3054.A191;
 
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class saveExcel {
+    public void saveData() {
+        try {
+            ScrapeData scrapeLinkData = new ScrapeData();
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            HSSFSheet sheet = workbook.createSheet("Link");
+        }
+
+    }
 }
