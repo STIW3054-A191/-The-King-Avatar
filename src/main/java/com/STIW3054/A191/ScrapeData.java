49d0ae6ba6e2d6025ded9df6069fb8708c560e71
Micky Phuah <55037983+phuahmeiqi@users.noreply.github.com>
2019-11-24 16:26:27 +0800

    Update

--- src/main/java/com/STIW3054/A191/ScrapeData.java
+++ src/main/java/com/STIW3054/A191/ScrapeData.java
@@ -13,5 +13,8 @@ public class ScrapeData
         final String url = "https://github.com/STIW3054-A191/Main-Issues/issues/1";
         final Document file = Jsoup.connect(url).get();
         Elements row = file.select("table");
+
+        return null;
     }
+
 }
