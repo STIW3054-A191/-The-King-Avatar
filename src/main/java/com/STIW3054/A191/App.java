c0fb4add42754c5110886cb3898e7880504f46f4
sohcheefung <gmail@sohcheefung>
2019-11-25 23:59:49 +0800

    update 259521

--- src/main/java/com/STIW3054/A191/App.java
+++ src/main/java/com/STIW3054/A191/App.java
@@ -1,15 +1,16 @@
 package com.STIW3054.A191;
 
 public class App {
+
     public void app() {
         try {
             ScrapeData save1 = new ScrapeData();
             save1.findAll();
 
-           System.out.println("\n\nSaving data to Excel...");
-           Thread.sleep(1000);// wait for 2 second
-           System.out.println("Excel Written Successfully");
-           System.out.println("END");
+            System.out.println("\n\nRunning");
+            Thread.sleep(1000);// wait for 2 second
+            System.out.println("Excel Written Successfully");
+            System.out.println("END");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
