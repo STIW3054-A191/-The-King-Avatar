511a908a770a40f22e8fd412a72980693425d995
weiditan <weiditan@hotmail.com>
2019-11-25 15:56:52 +0800

    259296 Update

--- src/main/java/com/STIW3054/A191/ScrapeData.java
+++ src/main/java/com/STIW3054/A191/ScrapeData.java
@@ -1,19 +1,14 @@
 package com.STIW3054.A191;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 import java.io.IOException;
-import java.util.Set;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Scanner;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
@@ -30,50 +25,53 @@ public class ScrapeData {
             System.out.printf("| %-10s| %-90s|\n", "No", "Link");
             System.out.println("----------------------------------------------------------------------------------------");
 
-            Elements linkdata = doc.select("table>tbody>tr>td");
-            for (int i = 1; i < linkdata.size(); i++) {
+            int i = 1;
+            ArrayList<String> arrLink = getLink();
+            for(String link:arrLink){
+                System.out.printf("| %-10s| %-90s|\n", i, link);
+                System.out.printf("| %-10s| %-90s|\n", " ", "Cloning " + link);
+                CloneRepo.clone(link);
+                System.out.printf("| %-10s| %-90s|\n", " ", "Completed Cloning");
+                i++;
+            }
 
-                Elements linkindata = linkdata.get(i).select("p");
-
-                for (int j = 0; j < linkindata.size(); j++) {
-                    String repoLink = null;
-
-                    Pattern link = Pattern.compile("https://.*");
-                    Matcher matchLink = link.matcher(linkindata.get(j).text());
-
-                    if (matchLink.find()) {
-                        System.out.printf("| %-10s| %-90s\n", i, matchLink.group());
-                        repoLink = matchLink.group();
-                    } else {
-                        System.out.print(" ");
-                    }
-
-                    String repoUrl = repoLink;
-                    String cloneDirectoryPath = ("D://git/");
-
-                    try {
-                        File file = new File(cloneDirectoryPath + i);
-                        file.mkdir();
-                        System.out.printf("| %-10s| %-90s|\n", " ", "Cloning " + repoUrl + " into folder " + i);
-                        Git.cloneRepository()
-                                .setURI(repoUrl)
-                                .setDirectory(file)
-                                .call();
-                        System.out.printf("| %-10s| %-90s|\n", " ", "Completed Cloning");
-                    } catch (GitAPIException e) {
-                        System.out.printf("Exception occurred while cloning");
-                        e.printStackTrace();
-                    }
 
                     //result.add(new Data(matchLink.group()));
-                }
-            }
+
             System.out.println("----------------------------------------------------------------------------------------");
             //return result;
 
         } catch (Exception e) {
             //return null;
         }
+    }
+
+    static String getRepoName(String repoUrl) {
+        String[] splitUrl = repoUrl.split("/");
+        return splitUrl[splitUrl.length-1];
+    }
+
+    private static ArrayList<String> getLink() {
+        // Get assignment 1 link
+        ArrayList<String> arrLink = new ArrayList<>();
+        try {
+            final Document document = Jsoup.connect("https://github.com/STIW3054-A191/Assignments/issues/1").get();
+            Elements elements = document.select("table").select("a");
+
+            for (Element element : elements) {
+                String elementLink = element.attr("href");
+
+                // Test .git link
+                if(elementLink.endsWith(".git")){
+                    arrLink.add(elementLink.substring(0, elementLink.length() - 4));;
+                }else {
+                    arrLink.add(elementLink);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return arrLink;
     }
 
 }
