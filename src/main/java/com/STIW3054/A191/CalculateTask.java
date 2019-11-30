e0d606b2b75aa625e5288b2fabede74136d98346
jasonway96 <limguangway96@gmail.com>
2019-11-30 10:33:02 +0800

    Update

---
+++ src/main/java/com/STIW3054/A191/CalculateTask.java
@@ -0,0 +1,21 @@
+package com.STIW3054.A191;
+
+import org.apache.poi.sl.draw.geom.Path;
+
+import java.io.File;
+
+public class CalculateTask
+{
+    private File outputFile;
+
+    private File classDir;
+
+    private Path extdirs;
+
+    private String format;
+
+    public CalculateTask()
+    {
+        this.format = "plain";
+    }
+}
