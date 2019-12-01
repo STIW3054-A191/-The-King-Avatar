c1911fad0db27908222a2facae06b6ab4a103b43
weiditan <weiditan@hotmail.com>
2019-12-01 10:20:44 +0800

    259296 Update

--- src/main/java/com/STIW3054/A191/Main.java
+++ src/main/java/com/STIW3054/A191/Main.java
@@ -6,23 +6,23 @@ import java.util.ArrayList;
 public class Main {
     public static void main (String[] args){
 
-        System.out.println("Checking folder...\n/target/repo/");
         // Delete /target/repo/ folder
+        System.out.println("Checking folder...\n/target/repo/");
         File file = new File(RepoPath.getPath());
         FileManager.deleteDir(file);
 
+        // Show total repositories
         System.out.println("\nCheck total Repositories...");
         ArrayList<String> arrLink = RepoLink.getLink();
         int totalRepo = arrLink.size();
         System.out.println("Total Repositories : "+totalRepo);
 
+        // Show PC total threads and threads use for cloning
         System.out.println("\nCheck PC total threads...");
         System.out.format("%-35s: %-20s\n","My PC total threads ", Threads.totalThreads());
         System.out.format("%-35s: %-20s\n","Total threads use for cloning ", Threads.availableThreads());
 
 
-        //App c_app = new App();
-        //c_app.app();
     }
 
 }
