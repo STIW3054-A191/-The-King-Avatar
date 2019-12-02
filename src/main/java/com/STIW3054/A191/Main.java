cb4f2274c6fdb0703ee24a0f06f2f0e563e7b8c8
weiditan <weiditan@hotmail.com>
2019-12-03 00:55:12 +0800

    259296 Update

--- src/main/java/com/STIW3054/A191/Main.java
+++ src/main/java/com/STIW3054/A191/Main.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
@@ -33,14 +34,14 @@ public class Main {
         // Show PC total threads and threads use for cloning
         System.out.println("\nCheck PC total threads...");
         System.out.format("%-35s: %-20s\n","My PC total threads ", Threads.totalThreads());
-        System.out.format("%-35s: %-20s\n","Total threads use for cloning ", Threads.availableThreads());
+        System.out.format("%-35s: %-20s\n","Total threads use for cloning ", Threads.availableLightThreads());
 
         // Cloning all repositories with threads
         System.out.println("\nCloning...");
         // Use CountDownLatch to check when all threads completed.
         CountDownLatch latch = new CountDownLatch(totalRepo);
         // Use ExecutorService to set max threads can run in same time. By using 3/4 from My PC total threads.
-        ExecutorService exec = Executors.newFixedThreadPool(Threads.availableThreads());
+        ExecutorService exec = Executors.newFixedThreadPool(Threads.availableLightThreads());
         // Create threads to cloning
         for(String link:arrLink){
             Thread thread = new Thread(new CloneRepoRunnable(link, totalRepo, latch));
@@ -55,7 +56,7 @@ public class Main {
             e.printStackTrace();
         }
 
-        System.out.println("Completed Cloning");
+        System.out.println("Cloning Completed !");
 
         // Set maven home for invoker used
         System.setProperty("maven.home", MavenHome.getPath());
@@ -64,6 +65,10 @@ public class Main {
         File repoDir = new File(RepoPath.getPath());
         String[] allRepo = repoDir.list();
         if(allRepo!=null) {
+
+            CountDownLatch latch2 = new CountDownLatch(allRepo.length);
+            ExecutorService exec2 = Executors.newFixedThreadPool(Threads.availableHeavyThreads());
+
             for (String repo : allRepo) {
 
                 File aRepoDir = new File(repoDir, repo);
@@ -72,6 +77,8 @@ public class Main {
                     //Check pom.xml file location
                     String pomPath = PomPath.getPath(aRepoDir);
                     if(pomPath!=null) {
+
+                        Thread thread = new Thread(() -> {
 
                         InvocationRequest request = new DefaultInvocationRequest();
                         request.setPomFile(new File(pomPath));
@@ -83,8 +90,7 @@ public class Main {
 
                         try {
                             if(invoker.execute( request ).getExitCode()==0){
-                                System.out.println(repo+" success");
-
+                                System.out.println(repo+" BUILD SUCCESS");
                             }else {
                                 System.err.println(repo+" BUILD FAILURE");
                             }
@@ -92,55 +98,71 @@ public class Main {
                             e.printStackTrace();
                         }
 
+                        latch2.countDown();
+
+                       });
+
+                        exec2.execute(thread);
+
 
                     }else {
-                        //For no maven file
+                        //For no pom.xml file
+
                         //Save error to log
                         try {
                             FileHandler fileHandler = new FileHandler(repoDir.getPath()+"/"+repoNameDetails.getMatric(repo)+".log",true);
                             fileHandler.setFormatter(new SimpleFormatter());
-
-                            Logger logger = Logger.getLogger(repo);
-                            logger.addHandler(fileHandler);
-                            logger.setUseParentHandlers(false);
-                            logger.warning(repo+" no pom.xml file.");
+                                Logger logger = Logger.getLogger(repo);
+                                logger.addHandler(fileHandler);
+                                logger.setUseParentHandlers(false);
+                                logger.warning(repo+" no pom.xml file.");
+                            fileHandler.close();
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
 
                         //Print error
                         System.err.println(repo + " no pom.xml file.");
+
+                        latch2.countDown();
                     }
+                }else {
+                    latch2.countDown();
                 }
             }
+
+            exec2.shutdown();
+            try {
+                latch2.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         //Get end time and time elapsed
         long endTime = System.currentTimeMillis();
         long timeElapsed = endTime - startTime;
-        System.out.println("\nExecution time in milliseconds: " + timeElapsed);
+
+        long days = TimeUnit.MILLISECONDS.toDays(timeElapsed);
+        long hours = TimeUnit.MILLISECONDS.toHours(timeElapsed) % 24;
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(timeElapsed) % 60 ;
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(timeElapsed) % 60 ;
+        long milliseconds = timeElapsed % 1000 ;
+
+        System.out.print("\nExecution time : ");
+
+        if(days!=0){
+            System.out.print(days+" Days, "+hours+" Hours, "+minutes+" Minutes, "+seconds+" Seconds, "+milliseconds+" Milliseconds.");
+        }else if(hours!=0){
+            System.out.print(hours+" Hours, "+minutes+" Minutes, "+seconds+" Seconds, "+milliseconds+" Milliseconds.");
+        }else if(minutes!=0){
+            System.out.print(minutes+" Minutes, "+seconds+" Seconds, "+milliseconds+" Milliseconds.");
+        }else if(seconds!=0){
+            System.out.print(seconds+" Seconds, "+milliseconds+" Milliseconds.");
+        }else {
+            System.out.print(milliseconds + " Milliseconds.");
+        }
+
     }
 }
