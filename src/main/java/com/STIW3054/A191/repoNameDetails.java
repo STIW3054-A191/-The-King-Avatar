4e836c0b6f6feb0e1322cab828c2997cdb4fc5fd
weiditan <weiditan@hotmail.com>
2019-12-13 17:11:03 +0800

    259296 Update

--- src/main/java/com/STIW3054/A191/repoNameDetails.java
+++
@@ -1,8 +0,0 @@
-package com.STIW3054.A191;
-
-public class repoNameDetails {
-    static String getMatric(String repoName) {
-        String[] splitRepoName = repoName.split("-");
-        return splitRepoName[0];
-    }
-}
