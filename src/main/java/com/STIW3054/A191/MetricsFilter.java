f560ce9c03aca5ed78b51dac306d8343d19bf19c
PhuahMeiQi <meiqi1106@gmail.com>
2019-12-29 14:39:56 +0800

    update code MetricsFilter

--- src/main/java/com/STIW3054/A191/MetricsFilter.java
+++ src/main/java/com/STIW3054/A191/MetricsFilter.java
@@ -28,6 +28,25 @@ public class MetricsFilter {
         int spc;
         JavaClass jc = null;
 
-
+        if ((spc = clspec.indexOf(' ')) != -1) {
+            String jar = clspec.substring(0, spc);
+            clspec = clspec.substring(spc + 1);
+            try {
+                jc = new ClassParser(jar, clspec).parse();
+            } catch (IOException e) {
+                System.err.println("Error loading " + clspec + " from " + jar + ": " + e);
+            }
+        } else {
+            try {
+                jc = new ClassParser(clspec).parse();
+            } catch (IOException e) {
+                System.err.println("Error loading " + clspec + ": " + e);
+            }
+        }
+        if (jc != null) {
+            ClassVisitor visitor = new ClassVisitor(jc, cm);
+            visitor.start();
+            visitor.end();
+        }
     }}
 
