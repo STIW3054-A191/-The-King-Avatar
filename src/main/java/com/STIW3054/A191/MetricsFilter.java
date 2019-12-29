0498c9e0ccedb4a226a594c7e64ef65e824b8fdc
PhuahMeiQi <meiqi1106@gmail.com>
2019-12-29 14:39:04 +0800

    update code MetricsFilter

---
+++ src/main/java/com/STIW3054/A191/MetricsFilter.java
@@ -0,0 +1,33 @@
+package com.STIW3054.A191;
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.generic.*;
+import org.apache.bcel.Repository;
+import org.apache.bcel.Constants;
+import org.apache.bcel.util.*;
+import java.io.*;
+import java.util.*;
+
+public class MetricsFilter {
+    /** True if the measurements should include calls to the Java JDK into account */
+    private static boolean includeJdk = false;
+
+    /** True if the reports should only include public classes */
+    private static boolean onlyPublic = false;
+
+    /** Return true if the measurements should include calls to the Java JDK into account */
+    public static boolean isJdkIncluded() { return includeJdk; }
+    /** Return true if the measurements should include all classes */
+    public static boolean includeAll() { return !onlyPublic; }
+
+    /**
+     * Load and parse the specified class.
+     * The class specification can be either a class file name, or
+     * a jarfile, followed by space, followed by a class file name.
+     */
+    static void processClass(ClassMetricsContainer cm, String clspec) {
+        int spc;
+        JavaClass jc = null;
+
+
+    }}
+
